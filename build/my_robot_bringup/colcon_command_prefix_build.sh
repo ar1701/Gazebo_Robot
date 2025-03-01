f968a072bd17660e47154ebf2982e3034170fb1e
Ayush Raj <ar898993@gmail.com>
2025-03-01 11:47:35 +0530

    Added all files

---
+++ build/my_robot_bringup/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/ayush/Desktop/ROS_part_2/install/my_robot_description/share/my_robot_description/package.sh"
---
+++ install/my_robot_bringup/share/my_robot_bringup/local_setup.zsh
@@ -0,0 +1,1 @@
+/home/ayush/Desktop/ROS_part_2/build/my_robot_bringup/ament_cmake_environment_hooks/local_setup.zsh
